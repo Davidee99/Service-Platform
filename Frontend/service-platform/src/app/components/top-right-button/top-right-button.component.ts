488107100c2d5bb7ac857da5ffee1744df04be0f
TirelliD04 <tiredavi@libero.it>
2024-02-07 16:55:30 +0100

    tatto 2

--- Frontend/service-platform/src/app/components/top-right-button/top-right-button.component.ts
+++ Frontend/service-platform/src/app/components/top-right-button/top-right-button.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 import { NgbActiveModal, NgbModal, NgbModalConfig } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-top-right-button',
   templateUrl: './top-right-button.component.html',
   styleUrls: ['./top-right-button.component.css'],
+  encapsulation: ViewEncapsulation.None,
+  styles: [`
+  .dark-modal .modal-content {
+      background-color: #292b2c;
+      color: white;
+  }
+  .dark-modal .close {
+      color: white;
+  }
+  .light-blue-backdrop {
+      background-color: #5cb3fd;
+  }`],
   providers: [NgbModalConfig, NgbModal]
 })
 export class TopRightButtonComponent {
@@ -19,6 +31,6 @@ export class TopRightButtonComponent {
 
 
 open(content: any) {
-    this.modalService.open(content);
+    this.modalService.open(content, {centered : true , modalDialogClass:"dark-modal"});
 }
 }
