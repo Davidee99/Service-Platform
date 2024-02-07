d2082fbe212e4c9cd32f9ba25190f0004785140e
Francesco142 <francesco.impellizzeri02@gmail.com>
2024-02-07 12:47:52 +0100

    per Michele e Giuseppe risolvete il required "object is possible null"

--- Frontend/service-platform/src/app/components/LandingPage/landing-page/landing-page.component.ts
+++ Frontend/service-platform/src/app/components/LandingPage/landing-page/landing-page.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
-import { FormControl, Validators } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-landing-page',
   templateUrl: './landing-page.component.html',
   styleUrls: ['./landing-page.component.css']
 })
-export class LandingPageComponent {
+export class LandingPageComponent{
+ 
+
+  userData = {
+    
+    firstname : "",
+    lastname : "",
+    email: "",
+    type: "",
+    message : ""
+
+  };
+  
+
+  
 
 
 
