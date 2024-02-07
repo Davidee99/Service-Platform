80a0627a7f13e25c9176395a2f3680d124a62a8d
sieg-feranil <127198356+sieg-feranil@users.noreply.github.com>
2024-02-07 15:40:23 +0100

    top-right-button

--- Frontend/service-platform/src/app/components/top-right-button/top-right-button.component.ts
+++ Frontend/service-platform/src/app/components/top-right-button/top-right-button.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
+import { NgbActiveModal, NgbModal, NgbModalConfig } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-top-right-button',
   templateUrl: './top-right-button.component.html',
-  styleUrls: ['./top-right-button.component.css']
+  styleUrls: ['./top-right-button.component.css'],
+  providers: [NgbModalConfig, NgbModal]
 })
 export class TopRightButtonComponent {
+  constructor(
+    config: NgbModalConfig,
+    private modalService: NgbModal,
+) {
+    // customize default values of modals used by this component tree
+    config.backdrop = 'static';
+    config.keyboard = false;
+}
 
+
+open(content: any) {
+    this.modalService.open(content);
+}
 }
