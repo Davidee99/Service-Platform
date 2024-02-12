ffcf53b18392e86a385dcf84fc99de9690a7bf29
sieg-feranil <127198356+sieg-feranil@users.noreply.github.com>
2024-02-12 14:16:47 +0100

    updated models

--- Frontend/service-platform/src/model/ticket.model.ts
+++ Frontend/service-platform/src/model/ticket.model.ts
@@ -1,6 +1,13 @@
-export interface Ticket  {
-    id:number,
-    message:string,
-    status:"NON_WIP"|"WIP"|"CLOSED",
-    order_id?:number
+export interface Ticket {
+  id: number;
+  message: string;
+  user_id?: number;
+  status: 'NON_WIP' | 'WIP' | 'CLOSED' | 'CANCELLED';
+  status_error?: 'DUPLICATED' | 'FAKE' | 'WRONG_SECTOR';
+  priority?: string;
+  create_date: string;
+  closing_date?: string;
+  type?: 'REFUND' | 'PRODUCT';
+  order_id?: number;
+  x:"3";
 }
---
+++ Frontend/service-platform/src/model/user-credentials.model.ts
@@ -0,0 +1,8 @@
+export interface UserCredential {
+    user_id:number;
+    firstname:string;
+    lastname:string;
+    role:"ADMIN"|"OPERATOR"|"USER";
+    token:string;
+    email:string;
+}
