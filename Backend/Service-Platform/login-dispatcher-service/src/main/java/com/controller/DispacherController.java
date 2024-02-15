9ce0b0923f2cc87ea15419bda1e087e15485c857
BigBeefs <bigbeefs98@gmail.com>
2024-02-15 10:39:28 +0100

    Cambio url

--- Backend/Service-Platform/login-dispatcher-service/src/main/java/com/controller/DispacherController.java
+++ Backend/Service-Platform/login-dispatcher-service/src/main/java/com/controller/DispacherController.java
@@ -6,7 +6,6 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,32 +15,19 @@ import org.springframework.web.client.RestTemplate;
 import com.configuration.URLS;
 
 @RestController
-@RequestMapping("/api/dispacher")
+@RequestMapping("/api/dispacher/")
 public class DispacherController {
 
 	@Autowired
 	RestTemplate restTemplate;
 
-	@GetMapping("/test")
-	ResponseEntity<?> test() {
-		String urlMailCustom = "http://localhost:5000/mailsender/test";
-		ResponseEntity<?> response;
-		try {
-			response = restTemplate.exchange(urlMailCustom, HttpMethod.GET,null, String.class);
-		} catch (RestClientException e) {
-			System.out.println(e);
-			return null;
-		}
-		return response;
-	}
-
-	@PostMapping("/mail-service/sendCustom")
+	@PostMapping("mail-service/sendCustom/")
 	ResponseEntity<?> sendCustom(HttpEntity<?> request) {
 		String url = URLS.SEND_CUSTOM_MAIL; 
 		return sampleRestTemplate(HttpMethod.POST,url,request);
 	}
 	
-	@PostMapping("/mail-service/sendDefault")
+	@PostMapping("mail-service/sendDefault/")
 	ResponseEntity<?> sendDefault(HttpEntity<?> request) {
 		String url = URLS.SEND_DEFAULT_MAIL; 
 		return sampleRestTemplate(HttpMethod.POST,url,request);
@@ -51,6 +37,8 @@ public class DispacherController {
 	
 	
 	// PRIVATE //
+	
+	private final String ACCESS_KEY = "querty";
 	
 	/**
 	 *  If it's a GET -> request = null
@@ -63,7 +51,7 @@ public class DispacherController {
 		//Utilizziamo questa chiave per evitare l'esposizione dei metodi del nostro servizio.
 		//Questo valore verra' controllato in ogni metodo del nostro servizio che passa per il dispatcher, 
 		//in caso di una incongruenza riceveremo uno status 401 UNAUTHORIZED 
-		headers.add("access_key","qwerty");
+		headers.add("access_key",ACCESS_KEY);
 		
 		HttpEntity<?> newEntity = new HttpEntity<>(request.getBody(),headers);
 		
