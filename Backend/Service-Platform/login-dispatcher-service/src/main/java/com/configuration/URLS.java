63b1847e65008e8f0c2a7bd20ff41dbc0f2715bf
BigBeefs <bigbeefs98@gmail.com>
2024-02-15 10:39:28 +0100

    Cambio url

--- Backend/Service-Platform/login-dispatcher-service/src/main/java/com/configuration/URLS.java
+++ Backend/Service-Platform/login-dispatcher-service/src/main/java/com/configuration/URLS.java
@@ -5,6 +5,6 @@ package com.configuration;
  * */
 public class URLS {
 
-	public static final String SEND_CUSTOM_MAIL = "http://localhost:5000/mailsender/sendCustom";
-	public static final String SEND_DEFAULT_MAIL = "http://localhost:5000/mailsender/sendDefault";
+	public static final String SEND_CUSTOM_MAIL = "http://localhost:5000/mailsender/sendCustom/";
+	public static final String SEND_DEFAULT_MAIL = "http://localhost:5000/mailsender/sendDefault/";
 }
